1aa964ca90b5a745f32d71ecce4af4ee4f4c8f03
iodaniel <iodaniel@icloud.com>
2022-07-18 15:52:39 -0600

    comentarios

--- week06_NASA/javascript/app.js
+++ week06_NASA/javascript/app.js
@@ -1,18 +1,18 @@
 const url = 'https://api.nasa.gov/planetary/apod?api_key='
 const api_key = config.NASA_API_KEY
 
-const fetchNASAData = async () => {
+const fetchNASAData = async () => {// fetchNASAData takes in no parameters and returns a promise}
   try {
-    const response = await fetch(`${url}${api_key}`)
+    const response = await fetch(`${url}${api_key}`) // fetch the data from the API
     const data = await response.json()
     console.log('NASA APOD data', data)
-    displayData(data)
+    displayData(data)// displayData is a function that takes in data and displays it on the page
   } catch (error) {
-    console.log(error)
+    console.log(error) //console.log the error if there is one
   }
 }
 
-const displayData = data => {
+const displayData = data => {// displayData takes in data and displays it on the page
   document.getElementById('title').textContent = data.title
   document.getElementById('date').textContent = data.date
   document.getElementById('picture').src = data.hdurl
@@ -20,5 +20,6 @@ const displayData = data => {
 }
 
 fetchNASAData()
+displayData()
 
 //conditional branchish
